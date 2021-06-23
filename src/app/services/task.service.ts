36884686ccc6834bd544818af2f384d572a8a78f
Arunav <arunav.sen@dimikit.com>
2021-06-23 15:34:46 +0600

    Add npm json server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { TaskList } from 'src/app/mock-task';
 import { ITask } from 'src/app/ITask';
-import { Observable, of} from 'rxjs';
+import { Observable} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
 
   getTask(): Observable<ITask[]> {
-    const tasks = of(TaskList);
-    return tasks;
+    return this.http.get<ITask[]>(this.apiUrl);
   }
 }
